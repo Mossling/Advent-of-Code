a00c9e33fc06a6866f6f7d69d412e6d5dff77358
Daniel Christianson <daniel.s.christianson@gmail.com>
2016-01-22 15:07:17 -0600

    Create day04-part01.js

---
+++ Day-04/Part-01/day04-part01.js
@@ -0,0 +1,12 @@
+md5 = require('js-md5')
+
+var input = "yzbqklnj"
+var num = 0
+var result = ''
+
+while (!result.startsWith('00000')){
+	result = md5(input + ++num)
+}
+
+console.log(result)
+console.log(num)
